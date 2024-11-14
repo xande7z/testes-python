5342645298abe3e0d03b103a509ee3b56e36304f
xande7z <alexandrejoasdiaa211@gmail.com>
2024-11-14 17:25:30 -0300

    Arquivo modificado

--- test_calculadoraIMC.py
+++ test_calculadoraIMC.py
@@ -9,6 +9,17 @@ class testCalculadoraIMC(unittest.TestCase):
     def testImc_magreza(self):
         self.assertEqual(self.calc.calcular_IMC(50, 1.80), 'magreza')
         
+        def testImc_normal(self):
+        self.assertEqual(self.calc.calcular_IMC(60, 1.60), 'normal')
+        
+        def testImc_sobrepeso(self):
+        self.assertEqual(self.calc.calcular_IMC(80, 1.60), 'sobrepeso')
+        
+        def testImc_obesidade(self):
+        self.assertEqual(self.calc.calcular_IMC(180, 1.60), 'obesidade')
+        
+        
+        
 
 if __name__ == '__main__':
     unittest.main()
